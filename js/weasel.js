1ed78d6a76b92ecd112bb32035af04070e0d66a1
Andrew Mauboussin <andrew@kensho.com>
2016-04-29 22:28:53 -0400

    basic visuals

--- js/weasel.js
+++ js/weasel.js
@@ -2,9 +2,9 @@
 var target = "METHINKS IT IS LIKE A WEASEL";
 var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
 var mutation_rate = 0.01;
-var n_organisms = 1000;
+var n_organisms = 10;
 var sexual_selection_factor = 0.0;
-var death_rate = 0.2;
+var survival_rate = 0.5;
 
 var strings = new Array(n_organisms).fill().map(random_string);
 
@@ -45,11 +45,11 @@ function fitness(s){
 
 function get_couples(){
     var couples = [];
-    for (var i = 0; i < 2; i++){
+    for (var i = 0; i < strings.length; i++){
         for (j = 0; j < strings.length; j++){
             if (i != j){
                 var remaining = (strings.length - j - 1);
-                match_prob = 1 / remaining + sexual_selection_factor * (remaining - 1)/ remaining;
+                var match_prob = 1 / remaining + sexual_selection_factor * (remaining - 1)/ remaining;
                 if (Math.random() < match_prob){
                     couples.push([i, j]);
                     break;
@@ -62,23 +62,86 @@ function get_couples(){
 
 function spawn_new_generation(){
     var new_organisms = [];
-    for (var i = 0; i < Math.ceil(1 / death_rate); i++){
+    for (var i = 0; i < Math.ceil(1 / survival_rate); i++){
         var couples = get_couples();
         new_organisms = new_organisms.concat(couples.map(mate));
     }
     new_organisms.sort(function (a, b){
         return fitness(a) < fitness(b) ? 1 : -1 ;
     });
+
     return new_organisms.slice(0, n_organisms);
 }
 
 function main(){
-    for (var i = 0; i < 1000; i++){
-    strings = spawn_new_generation();
-        if (i % 10 == 0)
-        console.log(strings[0], fitness(strings[0]));
+
+    function do_generation(iteration){
+        console.log(iteration)
+        strings = spawn_new_generation();
+        if (iteration % 10 === 0){
+            console.log(strings[0], fitness(strings[0]));
+            draw_organisms(strings);
+        }
+    }
+
+    for (var i = 0; i < 100; i++){
+        setTimeout(function(){do_generation(i);}, 500 * i);
     }
 }
+
+var width = 2000,
+    height = 800;
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", "translate(32," + (height / 2) + ")");
+
+var row_width = 400;
+var col_height = 15;
+var per_row = 5;
+var text;
+
+function get_x_pos(d, i){
+    return (i % per_row) * row_width;
+}
+
+function get_y_pos(d, i){
+    return  (Math.floor(i/per_row)) * col_height;
+}
+
+function draw_organisms(organisms){
+    text = svg.selectAll("text")
+      .data(organisms, function(d, i) {return i;});
+
+    text.attr("class", "update")
+    .transition()
+      .duration(750)
+      .attr("x", get_x_pos)
+      .text(function(d){ return d;});
+    text.enter().append("text")
+      .attr("class", "enter")
+      .attr("dy", ".35em")
+      .attr("x", get_x_pos)
+      .attr("y",get_y_pos)
+      // .style("fill-opacity", 1e-6)
+      .text(function(d) { return d; })
+    .transition()
+      .duration(750)
+      .style("fill-opacity", 1);
+
+    // text.exit()
+    //   .attr("class", "exit")
+    // .transition()
+    //   .duration(750)
+    //   .attr("y", 60)
+    //   .style("fill-opacity", 1e-6)
+    //   .remove();
+
+
+}
+
 
 main();
 
